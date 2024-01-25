512d67c6503343c91ecd6eda79aa68eddec7a422
Yvonne0413 <408079378@qq.com>
2024-01-25 03:54:40 -0800

    updated google login but will crash after click

--- src/utils/UserUtils.jsx
+++ src/utils/UserUtils.jsx
@@ -10,6 +10,7 @@ export const getUserInfoFromStore = async () => {
   let userInfo = await AsyncStorage.getItem(SecureStoreKeys.UserInfo);
   if (userInfo) {
     userInfo = JSON.parse(userInfo);
+    // We should make sure that this console.log doesn't get called very frequently
     console.log("getUserInfoFromStore", userInfo);
     return userInfo;
   }
@@ -20,95 +21,89 @@ export const onPressLogout = async () => {
   await AsyncStorage.removeItem(SecureStoreKeys.UserInfo);
 };
 
-
-export const onPressSignIn = async () => {
-  try {
-    // First, configure the Google Signin method
-    GoogleSignin.configure({
-      androidClientId: Config.ANDROID_CLIENT_ID,
-      iosClientId: Config.IOS_CLIENT_ID,
-      // Add other configuration parameters if needed
-    });
-
-    // Check for Play services, necessary on Android
-    await GoogleSignin.hasPlayServices();
-
-    // Sign in and get the user data from Google
-    const userInfo = await GoogleSignin.signIn();
-
-    // Structure the payload you need to send to your backend
-    const payload = {
-      idToken: userInfo.idToken,
-      email: userInfo.user.email,
-      source: "google",
-      sourceId: userInfo.user.id,
-      firstName: userInfo.user.givenName,
-      lastName: userInfo.user.familyName,
-    };
-
-    // Send the user info to your backend for verification and further processing
-    const response = await axios.post(APIs.USER_LOGIN, payload, {
-      headers: getHeader(userInfo.idToken),
-    });
-
-    // Handle the response from your backend
-    handleResponse(response, userInfo);
-
-    // Store the user information in AsyncStorage for later use
-    await setUserInfoInStore(userInfo);
-
-    // Return the user data and response
-    return { userInfo, response };
-  } catch (error) {
-    // Handle any errors, such as failed Google login or backend issues
-    handleError(error);
-
-    // You could re-throw the error or return it to be handled where the function is called
-    throw error;
-  }
-};
-
-
 export const setUserInfoInStore = async (UserInfo) => {
-  await AsyncStorage.setItem(
+  await SecureStore.setItemAsync(
     SecureStoreKeys.UserInfo,
     JSON.stringify(UserInfo)
   );
 };
 
-// ... rest of your existing code ...
+export const onPressSignIn = () => {
+  GoogleSignin.configure({
+    androidClientId: Config.ANDROID_CLIENT_ID,
+    iosClientId: Config.IOS_CLIENT_ID,
+  });
+
+  return new Promise((resolve, reject) => {
+    return GoogleSignin.hasPlayServices()
+      .then((hasPlayService) => {
+        if (hasPlayService) {
+          GoogleSignin.signIn()
+            .then((userInfo) => {
+              const headers = getHeader();
+              const { idToken, scopes, user } = userInfo;
+              axios
+                .post(
+                  APIs.USER_LOGIN,
+                  {
+                    idToken: idToken,
+                    email: user.email,
+                    source: "google",
+                    sourceId: user.id,
+                    firstName: user.givenName,
+                    lastName: user.familyName,
+                  },
+                  { headers }
+                )
+                .then((response) => {
+                  setUserInfoInStore(userInfo);
+                  handleResponse(response, userInfo);
+                  resolve({ response, userInfo });
+                })
+                .catch((e) => {
+                  handleError(e);
+                  reject(e);
+                });
+            })
+            .catch((e) => {
+              handleError(e);
+              reject(e);
+            });
+        }
+      })
+      .catch((e) => {
+        handleError(e);
+        reject(e);
+      });
+  });
+};
 
 export const getCart = async () => {
   try {
     let cart = await AsyncStorage.getItem(SecureStoreKeys.Cart);
+    cart = JSON.parse(cart);
     if (cart) {
-      return JSON.parse(cart);
+      return cart;
     }
     await AsyncStorage.setItem(SecureStoreKeys.Cart, JSON.stringify([]));
-    return [];
   } catch (err) {
     await AsyncStorage.setItem(SecureStoreKeys.Cart, JSON.stringify([]));
-    return [];
   }
-};
+}
 
 export const setCart = async (item, quantity) => {
   let cart = await AsyncStorage.getItem(SecureStoreKeys.Cart);
-  if (cart) {
-    cart = JSON.parse(cart);
-  } else {
-    cart = [];
-  }
+  cart = JSON.parse(cart);
   let found = false;
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].id === item) {
-      cart[i].quantity = quantity;
+      cart[i].quantity = quantity
       found = true;
       break;
     }
   }
   if (!found) {
-    cart.push({ id: item, quantity: quantity });
+    cart.push({id: item, quantity: quantity})
   }
   await AsyncStorage.setItem(SecureStoreKeys.Cart, JSON.stringify(cart));
 };
