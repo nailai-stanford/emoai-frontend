a9d00b5c542609693a6629b02b04fdf2b2e7298f
Yvonne0413 <408079378@qq.com>
2024-01-29 21:54:48 -0800

    local url

--- src/utils/Config.jsx
+++ src/utils/Config.jsx
@@ -1,7 +1,7 @@
 // TODO Refactor this file after we find a solution to react-native-config
 
 export const Config = {
-  BASE_URL: "https://nailanil.ai:8000",
+  BASE_URL: "http://127.0.0.1:8000",
   IOS_CLIENT_ID:
     "433081369336-ijjtbop5esp27v3h61qcdopbla1ifg0t.apps.googleusercontent.com",
 };
