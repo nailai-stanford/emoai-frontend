fd340740406b3a8d89e8602b6796cffeb296502c
Heidi <heidi3.1415926@gmail.com>
2024-02-23 19:04:40 -0800

    update baseURL

--- src/utils/Config.jsx
+++ src/utils/Config.jsx
@@ -2,7 +2,8 @@
 
 export const Config = {
   // BASE_URL: "http://192.168.1.231:8000",
-  BASE_URL: "https://api-test.e-m-o.ai",
+  // BASE_URL: "https://api-test.e-m-o.ai",
+  BASE_URL: "https://api.e-m-o.ai",
 
   IOS_CLIENT_ID:
     "433081369336-ijjtbop5esp27v3h61qcdopbla1ifg0t.apps.googleusercontent.com",
