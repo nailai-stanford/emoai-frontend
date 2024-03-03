ddeb630fceeb1dea4ddb09fb509fc39e3a80948d
张利高 <zhangligao9509@gmail.com>
2024-03-02 20:37:46 -0500

    default avatar

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -34,7 +34,7 @@ export const UserInfo = (props) => {
               borderRadius: avatarSize / 2,
             }}
             source={{
-              uri: localLogin && userInfo ? userInfo.user.photo : "https://emobackend.s3.amazonaws.com/app_assets/portrait-frog2.jpg",
+              uri: localLogin && userInfo && userInfo.user.photo ? userInfo.user.photo : "https://emobackend.s3.amazonaws.com/app_assets/portrait-frog2.jpg",
             }}
           />
         </View>
